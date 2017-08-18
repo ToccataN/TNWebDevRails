ca0290a667d408b2185fa625c1869c03acf88656
ToccataN <charlestonshadowsx@gmail.com>
2017-08-18 15:50:00 -0400

    mailer functionality added in for contact form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options =
      { :host => 'https://toccata-nosferatu.herokuapp.com'}
 
-     config.action_mailer.delivery_method = :smtp
+  Rails.application.routes.default_url_options[:host] =
+      'https://toccata-nosferatu.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
    config.action_mailer.raise_delivery_errors = false
    config.action_mailer.default :charset => "utf-8"
