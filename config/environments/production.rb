be69211615669d265c77ba26ed04e800b79feefe
ToccataN <charlestonshadowsx@gmail.com>
2017-08-18 20:56:59 -0400

    building mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
    config.action_mailer.smtp_settings = {
    address: "smtp.gmail.com",
    port: 587,
-   domain: 'https://toccata-nosferatu.herokuapp.com',
-   authentication: :plain,
+   domain: 'toccata-nosferatu.herokuapp.com',
+   authentication: 'plain',
    enable_starttls_auto: true,
    user_name: ENV["GMAIL_USERNAME"],
    password: ENV["GMAIL_PASSWORD"]
