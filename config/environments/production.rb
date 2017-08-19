36e9b52a7ff9e9903880eeb69f1ec04f65e4146a
ToccataN <charlestonshadowsx@gmail.com>
2017-08-19 14:00:16 -0400

    building mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,15 +72,17 @@ Rails.application.configure do
    config.action_mailer.raise_delivery_errors = false
    config.action_mailer.default :charset => "utf-8"
 
-  MailForm::Base.smtp_settings = {
-   :port           => ENV['MAILGUN_SMTP_PORT'],
-   :address        => ENV['MAILGUN_SMTP_SERVER'],
-   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-   :domain         => 'toccata-nosferatu.herokuapp.com',
-   :authentication => :plain,
- }
- MailForm::Base.delivery_method = :smtp,
+   config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+   domain: 'toccata-nosferatu.herokuapp.com',
+   authentication: 'plain',
+   enable_starttls_auto: true,
+
    }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
